a71d824d0e6ddf6026666296205e81519deb6f69
anhthu1402 <20520792@gm.uit.edu,vn>
2023-12-18 23:03:15 +0700

    first commit

---
+++ src/main/java/Nhom5_QLMH_API/Nhom5QlmhApiApplication.java
@@ -0,0 +1,13 @@
+package Nhom5_QLMH_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom5QlmhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom5QlmhApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Nhom5_QLMH_API/Nhom5QlmhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Nhom5_QLMH_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom5QlmhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
